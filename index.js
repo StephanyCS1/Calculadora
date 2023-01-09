8f723469e828e94e0c5e4e97035f56bcc90da4fb
StephanyCS1 <stephanycastrosalas@gmail.com>
2023-01-08 21:49:29 -0500

    Vista del calculo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', () => { //empieza a escuchar al cargar el doc
 
-    const display = document.querySelector('.display');
-    const resultView = document.querySelector('result');
+    const display = document.querySelector('.calcu');
+    const resultView = document.querySelector('.result');
     //crea un html collection con todos los botones
     const buttons = document.getElementsByClassName('button');
     //creamos un array del html collection
@@ -10,14 +10,14 @@ window.addEventListener('load', () => { //empieza a escuchar al cargar el doc
     //iterar el nuevo array
     buttonsArray.forEach((button) => {
         button.addEventListener('click', () => {
-            options(button, display);
+            options(button, display, resultView);
         });
     })
 
-    options = (button, display) => {
+    options = (button, display, resultView) => {
         switch (button.innerHTML) {
             case 'AC':
-                cleanView(display);
+                cleanView(display, resultView);
                 break;
             case '%':
                 percent(display);
@@ -26,28 +26,29 @@ window.addEventListener('load', () => { //empieza a escuchar al cargar el doc
                 postNeg(display)
                 break;
             case '=':
-                result(display)
+                result(display,resultView)
                 break;
             default:
-                uptdate(display, button)
+                uptdate(display,button)
                 break;
         }
     }
 
-    result = (display) => {
+    result = (display, resultView) => {
         let value = display.innerHTML;
         let result = eval(value.toString());
         display.innerHTML = result;
+        resultView.innerHTML = value;
     }
 
     uptdate = (display, button) => {
         if (display.innerHTML == 0) display.innerHTML = '';
-
         display.innerHTML += button.innerHTML;
     }
 
-    cleanView = (display) =>{
+    cleanView = (display, resultView) =>{
         display.innerHTML = '';
+        resultView.innerHTML  = '';
     }
 
     percent = (display ) => {
@@ -55,7 +56,7 @@ window.addEventListener('load', () => { //empieza a escuchar al cargar el doc
             display.innerHTML = parseFloat(display.innerHTML/100);
         };
     }
-    postNeg = (display,) =>{
+    postNeg = (display) =>{
         display.innerHTML = - display.innerHTML;
     }
 
